adf76b1be29b3acdd6ab6f41c76a43b62af0674e
Pavel Lamov <git.habov@yandex.ru>
2021-03-05 17:20:15 +0300

    2,3 note

--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -5,34 +5,64 @@ import jm.task.core.jdbc.model.User;
 
 import java.util.List;
 
-public class UserServiceImpl extends UserDaoJDBCImpl implements UserService {
+//public class UserServiceImpl extends UserDaoJDBCImpl implements UserService {
 
-    public UserServiceImpl() {
-        super();
-    }
+    //public UserServiceImpl() {
+        //super();
+   // }
 
+
+    //public void createUsersTable() {
+        //super.createUsersTable();
+    //}
+
+    //public void dropUsersTable() {
+        //super.dropUsersTable();
+    //}
+
+    //public void saveUser(String name, String lastName, byte age) {
+        //super.saveUser(name, lastName, age);
+    //}
+
+    //public void removeUserById(long id) {
+        //super.removeUserById(id);
+    //}
+
+    //public List<User> getAllUsers() {
+        //return super.getAllUsers();
+    //}
+
+    //public void cleanUsersTable() {
+        //super.cleanUsersTable();
+    //}
+//}
+
+public class UserServiceImpl implements UserService {
+
+    UserDaoJDBCImpl daoJDBC = new UserDaoJDBCImpl();
 
     public void createUsersTable() {
-        super.createUsersTable();
+        daoJDBC.createUsersTable();
     }
 
     public void dropUsersTable() {
-        super.dropUsersTable();
+        daoJDBC.dropUsersTable();
     }
 
     public void saveUser(String name, String lastName, byte age) {
-        super.saveUser(name, lastName, age);
+        daoJDBC.saveUser(name, lastName, age);
     }
 
     public void removeUserById(long id) {
-        super.removeUserById(id);
+        daoJDBC.removeUserById(id);
     }
 
     public List<User> getAllUsers() {
-        return super.getAllUsers();
+        return daoJDBC.getAllUsers();
     }
 
     public void cleanUsersTable() {
-        super.cleanUsersTable();
+        daoJDBC.cleanUsersTable();
     }
+
 }
