cd4119882d41977edd8ec7fcca95c4035017ff19
Pavel Lamov <git.habov@yandex.ru>
2021-03-08 09:31:10 +0300

    adjustments

--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -5,38 +5,6 @@ import jm.task.core.jdbc.model.User;
 
 import java.util.List;
 
-//public class UserServiceImpl extends UserDaoJDBCImpl implements UserService {
-
-    //public UserServiceImpl() {
-        //super();
-   // }
-
-
-    //public void createUsersTable() {
-        //super.createUsersTable();
-    //}
-
-    //public void dropUsersTable() {
-        //super.dropUsersTable();
-    //}
-
-    //public void saveUser(String name, String lastName, byte age) {
-        //super.saveUser(name, lastName, age);
-    //}
-
-    //public void removeUserById(long id) {
-        //super.removeUserById(id);
-    //}
-
-    //public List<User> getAllUsers() {
-        //return super.getAllUsers();
-    //}
-
-    //public void cleanUsersTable() {
-        //super.cleanUsersTable();
-    //}
-//}
-
 public class UserServiceImpl implements UserService {
 
     UserDaoJDBCImpl daoJDBC = new UserDaoJDBCImpl();
