d5ca008d8b58c1aaf44837ea8f76da13091a7e45
Pavel Lamov <git.habov@yandex.ru>
2021-03-08 09:53:14 +0300

    adjustments

--- src/main/java/jm/task/core/jdbc/model/User.java
+++ src/main/java/jm/task/core/jdbc/model/User.java
@@ -1,103 +1,70 @@
 package jm.task.core.jdbc.model;
 
-import jm.task.core.jdbc.dao.UserDao;
+import javax.persistence.*;
 
-import javax.persistence.Column;
-import javax.persistence.Id;
-import javax.persistence.Table;
-import java.util.List;
+    @Entity
+    @Table(name="user")
+    public class User {
 
-@Table
-public class User implements UserDao {
-    @Id
-    private Long id;
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        @Column(name="id")
+        private Long id;
 
-    @Column
-    private String name;
+        @Column(name = "name")
+        private String name;
 
-    @Column
-    private String lastName;
+        @Column(name="last_name")
+        private String lastName;
 
-    @Column
-    private Byte age;
+        @Column(name="age")
+        private Byte age;
 
-    public User() {
+        public User() {
 
+        }
+
+        public User(String name, String lastName, Byte age) {
+            this.name = name;
+            this.lastName = lastName;
+            this.age = age;
+        }
+
+        public Long getId() {
+            return id;
+        }
+
+        public void setId(Long id) {
+            this.id = id;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getLastName() {
+            return lastName;
+        }
+
+        public void setLastName(String lastName) {
+            this.lastName = lastName;
+        }
+
+        public Byte getAge() {
+            return age;
+        }
+
+        public void setAge(Byte age) {
+            this.age = age;
+        }
+
+        @Override
+        public String toString() {
+            return "User{" + "id=" + id + ", name='" + name + '\'' +
+                    ", lastName='" + lastName + '\'' + ", age=" + age + '}';
+        }
     }
-
-    public User(String name, String lastName, Byte age) {
-        this.name = name;
-        this.lastName = lastName;
-        this.age = age;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public Byte getAge() {
-        return age;
-    }
-
-    public void setAge(Byte age) {
-        this.age = age;
-    }
-
-    @Override
-    public String toString() {
-        return getClass().getSimpleName() + " { id: " + id +
-                " username: " + name +
-                " lastname: " + lastName +
-                " age: " + age + "}";
-    }
-
-    @Override
-    public void createUsersTable() {
-
-    }
-
-    @Override
-    public void dropUsersTable() {
-
-    }
-
-    @Override
-    public void saveUser(String name, String lastName, byte age) {
-
-    }
-
-    @Override
-    public void removeUserById(long id) {
-
-    }
-
-    @Override
-    public List<User> getAllUsers() {
-        return null;
-    }
-
-    @Override
-    public void cleanUsersTable() {
-
-    }
-}
